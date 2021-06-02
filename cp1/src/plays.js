37415533798856b2487b6d52c6e90992b74be7f3
hayeon123 <selee3711@gmail.com>
2021-06-02 10:37:42 +0900

    [feat] statement 구현

---
+++ cp1/src/plays.js
@@ -0,0 +1,14 @@
+export default {
+  hamlet: {
+    name: "Hamlet",
+    type: "tragedy",
+  },
+  "as-like": {
+    name: "As You Like it",
+    type: "comedy",
+  },
+  othello: {
+    name: "Othello",
+    type: "tragedy",
+  },
+};
