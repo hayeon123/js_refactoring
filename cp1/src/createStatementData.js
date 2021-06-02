880868cc2c471bc0facb76ef8e27b07c4ab9f650
hayeon123 <selee3711@gmail.com>
2021-06-02 16:02:26 +0900

    [refactor] performanceCalculator로 다형성 적용하기

--- cp1/src/createStatementData.js
+++ cp1/src/createStatementData.js
@@ -7,10 +7,14 @@ export default function createStatementData(inovice, plays) {
   return statementData;
 
   function enrichPerformance(aPerformance) {
+    const calculator = new PerformanceCalculator(
+      aPerformance,
+      playFor(aPerformance)
+    );
     const result = Object.assign({}, aPerformance);
     result.play = playFor(result);
-    result.amount = amountFor(result);
-    result.volumeCredits = volumeCreditsFor(result);
+    result.amount = calculator.amount;
+    result.volumeCredits = calculator.volumeCredits;
     return result;
   }
 
@@ -19,41 +23,57 @@ export default function createStatementData(inovice, plays) {
   }
 
   function amountFor(aPerformance) {
-    let result = 0;
-    switch (aPerformance.play.type) {
-      case "tragedy":
-        result = 40000;
-        if (aPerformance.audience > 30) {
-          result += 1000 * (aPerformance.audience - 30);
-        }
-        break;
-      case "comedy":
-        result = 30000;
-        if (aPerformance.audience > 20) {
-          result += 10000 * (aPerformance.audience - 20);
-        }
-        result += 300 * aPerformance.audience;
-        break;
-
-      default:
-        throw new Error(`알 수 없는 장르: ${aPerformance.play.type}`);
-    }
-    return result;
+    return new PerformanceCalculator(aPerformance, playFor(aPerformance))
+      .amount;
   }
 
   function volumeCreditsFor(aPerformance) {
-    let result = 0;
-    //포인트 적립
-    result += Math.max(aPerformance.audience - 30, 0);
-    //희극 관객 5명마다 추가 포인트 제공
-    if ("comedy" === aPerformance.play.type)
-      result += Math.floor(aPerformance.audience / 5);
-    return result;
+    return new PerformanceCalculator(aPerformance, playFor(aPerformance))
+      .volumeCredits;
   }
   function totalAmount(data) {
     return data.performances.reduce((total, p) => total + p.amount, 0);
   }
   function totalVolumeCredits(data) {
     return data.performances.reduce((total, p) => total + p.volumeCredits, 0);
+  }
+}
+
+class PerformanceCalculator {
+  constructor(aPerformance, aPlay) {
+    this.performance = aPerformance;
+    this.play = aPlay;
+  }
+  get amount() {
+    let result = 0;
+    switch (this.play.type) {
+      case "tragedy":
+        result = 40000;
+        if (this.performance.audience > 30) {
+          result += 1000 * (this.performance.audience - 30);
+        }
+        break;
+      case "comedy":
+        result = 30000;
+        if (this.performance.audience > 20) {
+          result += 10000 * (this.performance.audience - 20);
+        }
+        result += 300 * this.performance.audience;
+        break;
+
+      default:
+        throw new Error(`알 수 없는 장르: ${this.play.type}`);
+    }
+    return result;
+  }
+
+  get volumeCredits() {
+    let result = 0;
+    //포인트 적립
+    result += Math.max(this.performance.audience - 30, 0);
+    //희극 관객 5명마다 추가 포인트 제공
+    if ("comedy" === this.play.type)
+      result += Math.floor(this.performance.audience / 5);
+    return result;
   }
 }
--- cp1/src/statement.js
+++ cp1/src/statement.js
@@ -18,6 +18,24 @@ function renderPlainText(data, plays) {
   result += `적립 포인트: ${data.totalVolumeCredits}점\n`;
   return result;
 }
+function htmlStatement(inovices, play) {
+  return renderHtml(createStatementData(inovices, play));
+}
+function renderHtml(data) {
+  let result = `<h1>청구 내역 (고객명: ${data.customer})</h1>\n`;
+  result += "<table>\n";
+  result += "<tr><th>연극</th><th>좌석수</th><th>금액</th></tr>";
+  for (let perf of data.performances) {
+    //청구 내역을 출력한다.
+    result += ` <tr><td>${perf.play.name}</td><td>(${
+      perf.audience
+    }석) </td><td>${usd(perf.amount)}</td>\n`;
+  }
+  result += "</table>\n";
+  result += `<p>총액: <em>${usd(data.totalAmount)}</em></p>\n`;
+  result += `<p>적립 포인트:<em> ${data.totalVolumeCredits}점</em></p>\n`;
+  return result;
+}
 function usd(aNumber) {
   return new Intl.NumberFormat("en-US", {
     style: "currency",
@@ -26,3 +44,4 @@ function usd(aNumber) {
   }).format(aNumber / 100);
 }
 console.log(statement(inovices, plays));
+htmlStatement(inovices, plays);
